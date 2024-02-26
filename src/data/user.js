be019a28bccaf72b49fe01b4edd56148d4022d44
Shimmita <douglasshimita3@gmail.com>
2024-02-26 14:33:17 +0300

    updated the responsiveness of the site for both mobile and desktop view

--- src/data/user.js
+++ src/data/user.js
@@ -33,9 +33,9 @@ const INFO = {
 
 	projects: [
 		{
-			title: "Blood-Bank-Management-Sysytem",
+			title: "Blood Bank MS",
 			description:
-				"React based web application powered by Node.js Express backend for optimatisation of blood bank activities such as keeping inventories of blood donors, recipients,keeping track of current blood bank levels,scheduling and managing of donor appointments. Skills: Firebase, Java, Kotlin.",
+				"React based web application powered by Node.js Express backend for optimatisation of blood bank activities such as keeping inventories of blood donors, recipients,keeping track of current blood bank levels,scheduling and managing of donor appointments. Skills:React.js, Redux, Bootstrap, Node.js, Github.",
 			logo: "https://banner2.cleanpng.com/20180720/kha/kisspng-react-javascript-library-redux-user-interface-tesseract-5b52265cc83931.4218499515321104288201.jpg",
 			linkText: "github",
 			link: "https://github.com/Shimmita/Blood-Bank-Management-System",
@@ -44,7 +44,7 @@ const INFO = {
 		{
 			title: "JobsAPI254",
 			description:
-				"Facilitates users of the api to easily locate jobs from a collection of jobs provided. Currently the API is meant to locate Techjobs but with time it will scale to provide other jobs too. Skils: Node.js Express and MongoDB, Mongo Atlas, API Hosting.",
+				"Facilitates users of the api to easily locate jobs from a collection of jobs provided. Currently the API is meant to locate Techjobs but with time it will scale to provide other jobs too. Skils: Node.js Express and MongoDB, Mongo Atlas, API Hosting, Github",
 			logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTu7zQwsBdrzNEq0Wyk2PzHHbCxT8fafhH7Q_Sd91kwjg&s",
 			linkText: "github",
 			link: "https://github.com/Shimmita/jobsAPI254 ",
@@ -53,7 +53,7 @@ const INFO = {
 		{
 			title: "JobWave ",
 			description:
-				"JobWave is a cutting-edge Android app built with Jetpack Compose, tailored for professionals in the IT field to easily locate IT related jobs and apply for them in an ABC way. Skils: JetpakCompose, Retrofit, Node.js Express and MongoDB.",
+				"JobWave is a cutting-edge Android app built with Jetpack. Its Backend is Node.js. It's tailored for professionals in the IT field to easily locate IT related jobs and apply for them in an ABC way. Skils: JetpakCompose, Retrofit,Kotlin, Github",
 			logo: "https://icons.iconarchive.com/icons/martz90/circle/512/android-icon.png",
 			linkText: "github",
 			link: "https://github.com/Shimmita/JobWave",
@@ -62,42 +62,42 @@ const INFO = {
 		{
 			title: "Market CM",
 			description:
-				"Android based marketing application that facilitates its registered users to easily buy or  sell their products online. The project targets students alias comrades who are in the universities or collages to market their second hand products within their versity arena. Skils: Java, Kotlin, Coroutines, Firebase.",
+				"Android based marketing application that facilitates its registered users to easily buy or  sell their products online. The project targets students alias comrades who are in the universities or collages to market their second hand products within their versity arena. Skils: Java, Kotlin, Coroutines, Firebase, Github.",
 			logo: "https://icons.iconarchive.com/icons/martz90/circle/512/android-icon.png",
 			linkText: "github",
 			link: "https://github.com//Shimmita/Marketing-App",
 		},
 
 		{
-			title: "School-Management-System ",
+			title: "School MS ",
 			description:
-				"Android Based Mobile Application whic helps in automation of various school Activities thus improving on the performance of the students and overal school output. Skills: Java, Kotlin, Firebase.",
+				"Android Based Mobile Application whic helps in automation of various school Activities thus improving on the performance of the students and overal school output. Skills: Java, Kotlin, Firebase, Github.",
 			logo: "https://icons.iconarchive.com/icons/martz90/circle/512/android-icon.png",
 			linkText: "github",
 			link: "https://github.com//Shimmita/School-Management-System.",
 		},
 
 		{
-			title: "paperpedia_flutter ",
+			title: "Paperpedia ",
 			description:
-				"Forget the library scramble! Paperpedia, your sleek Flutter app, grants instant access to a vast library of past papers. Paperpedia is your one-stop shop for conquering exams. Skills: Dart, Flutter, SQlite, Firebase.",
+				"Forget the library scramble! Paperpedia, your sleek Flutter app, grants instant access to a vast library of past papers. Paperpedia is your one-stop shop for conquering exams. Skills: Dart, Flutter, SQlite, Firebase, Github.",
 			logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTfHQVpYgAxPCQUuu5hq2hmF9M1HSnW0pBn_c8JXl4NiO0FcXlnYFWGocaCybO0NEGJc7w&usqp=CAU",
 			linkText: "github",
 			link: "https://github.com//Shimmita/paperpedia_flutter",
 		},
 
 		{
-			title: "Django_REST_Framwork App ",
+			title: "Django_REST_CRUD ",
 			description:
-				"Applying the concepts learnt from Python Django REST framework into practical. The concepts of Serialisation, using the Mixins and Generics in creating Restful APIs in a fastest way. Learning about Authentication and its imporntance of protecting routes from unauthorised access.Skills:Python, Django REST, MySQL.",
+				"Applying the concepts learnt from Python Django REST framework into practical. The concepts of Serialisation, using the Mixins and Generics in creating Restful APIs in a fastest way. Learning about Authentication and its imporntance of protecting routes from unauthorised access.Skills:Python, Django REST, MySQL, Github.",
 			logo: "https://pnghq.com/wp-content/uploads/how-to-create-a-python-png-with-a-transparent-background-2.png",
 			linkText: "github",
 			link: "https://github.com//Shimmita/django_REST_framwork_lab ",
 		},
 		{
-			title: "E-Commerce App Vanilla-PHP ",
+			title: "E-Commerce App",
 			description:
-				"Responsive Web app for marketing products globally. Main code base is vanilla PHP. Used MysQL database for storage and retrieval of data. This was my school project for mastering PHP. Skills: PHP, MySQL.",
+				"Responsive Web app for marketing products globally. Main code base is vanilla PHP. Used MysQL database for storage and retrieval of data. This was my school project for mastering PHP. Skills: PHP, MySQL, Github.",
 			logo: "https://w7.pngwing.com/pngs/751/3/png-transparent-logo-php-html-others-text-trademark-logo-thumbnail.png",
 			linkText: "github",
 			link: "https://github.com//Shimmita/Marketing-App-Vanilla-PHP ",
@@ -105,8 +105,17 @@ const INFO = {
 		{
 			title: "ChurchEvangelists ",
 			description:
-				"jetpak based android application built for managing church activities such as church membership registration and management, tracking of upcoming church events etc. Skills: Java, Kotlin, JetpakCompose Firebase.",
+				"jetpak based android application built for managing church activities such as church membership registration and management, tracking of upcoming church events etc. Skills: Java, Kotlin, JetpakCompose Firebase, Github.",
 			logo: "https://icons.iconarchive.com/icons/martz90/circle/512/android-icon.png",
+			linkText: "github",
+			link: "https://github.com//Shimmita/hurchEvangelists ",
+		},
+
+		{
+			title: "Potfolio",
+			description:
+				"A React.js FrontEnd digital potfolio which i used to develop my potfolio with, yes this potflio! and hosted it on Vercel. during the development I honed my FrontEnd development of using React.js and more other advanced concepts. Skill: React.js, Hosting Web App, Github.",
+			logo: "https://banner2.cleanpng.com/20180720/kha/kisspng-react-javascript-library-redux-user-interface-tesseract-5b52265cc83931.4218499515321104288201.jpg",
 			linkText: "github",
 			link: "https://github.com//Shimmita/hurchEvangelists ",
 		},
