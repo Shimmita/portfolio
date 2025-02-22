70fa088f68413653582aa17c70fe3bbfbfcd87c2
Shimmita <douglasshimita3@gmail.com>
2025-02-22 15:54:23 +0300

    updated the articles dates

--- src/data/articles.js
+++ src/data/articles.js
@@ -154,7 +154,7 @@ const machineLearningBenefits = [
 
 function article_1() {
 	return {
-		date: "20 Feb 2023",
+		date: "20 Jan 2025",
 		title: "The Benefits of Cloud Computing",
 		description:
 			"Cloud computing offers a range of benefits, including cost savings and increased flexibility. Find out why more businesses are turning to the cloud.",
@@ -203,7 +203,7 @@ function article_1() {
 
 function article_2() {
 	return {
-		date: "7 May 2023",
+		date: "20 Jan 2025",
 		title: "Artificial In Intelligence HealthCare Centers and Many Other Institution",
 		description:
 			"AI is transforming the healthcare industry, from improving patient outcomes to streamlining operations. Discover the latest applications of this game-changing technology.",
